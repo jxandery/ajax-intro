884287de43ccb1b3f6bf8d9b90b0bc9c786dca2f
Jack Yeh <jack.yeh@gmail.com>
2015-08-04 09:22:05 -0600

    Demonstrates AJAX 'GET' request: Using jQuery library loaded in the index.html: - define an ajax function - send a 'Get' request to the turing-birdie.herokuapp.com url - if the response is successful execute the 'posts' function - the 'posts' function returns/console_logs a table of successful posts

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://turing-birdie.herokuapp.com/api/v1/posts.json',
+    success: function(posts){
+      console.table(posts)
+    }
+  })
+})
