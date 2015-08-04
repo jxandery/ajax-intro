58f6fe0ab71fba3c918b0dbe07de6ef34100da62
Jack Yeh <jack.yeh@gmail.com>
2015-08-04 11:04:05 -0600

    Demonstrates an AJAX 'POST' request: - when the create button is clicked - if posting the postParams is successful execute the append functionality

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -4,18 +4,41 @@ $(document).ready(function() {
     url: 'http://turing-birdie.herokuapp.com/api/v1/posts.json',
     success: function(posts){
       $.each(posts, function(index, post){
-       $('#latest-posts').append(
-          "<div class-'post' data-id='"
-          + post.id
-          + "'><h6>Published on "
-          + post.created_at
-          + "</h6>"
-          + "<p>"
-          + post.description
-          + "</p>"
-          + "</div>"
-           )
+        $('#latest-posts').append(
+            "<div class='post' data-id='"
+            + post.id
+            + "'><h6>Published on "
+            + post.created_at
+            + "</h6>"
+            + "<p>"
+            + post.description
+            + "</p>"
+            + "</div>"
+            )
       })
     }
+  })
+
+  $('#create-post').on('click', function(){
+    var postParams = { post: {description: $('#post-description').val() } }
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://turing-birdie.herokuapp.com/api/v1/posts.json',
+      data: postParams,
+      success: function(newPost){
+        $('#latest-posts').append(
+            "<div class='post' data-id='"
+            + newPost.id
+            + "'><h6>Published on "
+            + newPost.created_at
+            + "</h6>"
+            + "<p>"
+            + newPost.description
+            + "</p>"
+            + "</div>"
+            )
+      }
+    })
   })
 })
